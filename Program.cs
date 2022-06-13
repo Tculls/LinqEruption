be1fb3a5d54d4a60872edce2b750c40064a3589d
Tculls <tanner.culley@gmail.com>
2022-06-13 13:16:39 -0700

    finish

--- Program.cs
+++ Program.cs
@@ -17,11 +17,46 @@
 // Example Query - Prints all Stratovolcano eruptions
 // IEnumerable<Eruption> stratovolcanoEruptions = eruptions.Where(c => c.Type == "Stratovolcano");
 // Use Linq to find the first eruption that is in CHile
-IEnumerable<Eruption> chileEruption = eruptions.Where(p => p.Location == "Chile");
+// IEnumerable<Eruption> chileEruption = eruptions.Where(p => p.Location.Contains( "Chile"));
+Eruption? cErupt = eruptions.FirstOrDefault(p => p.Location == "Chile");
 
+// Find Hawaiian Is eruption, if non pringt no hawaiian is found
+IEnumerable<Eruption> hawaiiIsland = eruptions.Where(h => h.Location == "Hawaiian Is");
+Console.WriteLine(hawaiiIsland.FirstOrDefault()!=null?("Hawaiian Is" + hawaiiIsland.First().Volcano): "No Hawaiian Is Eruption found");
+
+
+// Find eruption after year 1900 AND is in New Zealand
+IEnumerable<Eruption> nz1900 = eruptions.Where(nz => nz.Location =="New Zealand" && nz.Year > 1900);
+
+// All eruptions over 2000m
+IEnumerable<Eruption> over2000 = eruptions.Where(e => e.ElevationInMeters > 2000);
+
+// All eruptions wehre volcano name starts with Z
+IEnumerable<Eruption> zVolcano = eruptions.Where(z => z.Volcano.StartsWith("Z"));
+;
+
+// Highest elevation and print only that integer
+int maxElevation = eruptions.Max(eruption => eruption.ElevationInMeters);
+Eruption? tallestVolcano = eruptions.FirstOrDefault(big => big.ElevationInMeters == maxElevation);
+// Use the highest elevation variable to print name of volcano with elevation
+// height? tallestVolcano = eruptions.FirstOrDefault(volcano => volcano.ElevationInMeters = maxElevation);
+
+// Print volcanos names alphabetically
+IEnumerable<Eruption> abcVolcano = eruptions.OrderBy(abc=>abc.Volcano).ToList();
+
+
+// Print all eruptions that happened before the year 1000CE alpabetically by namne
+IEnumerable<Eruption> oldAbcVolcano = eruptions.Where(a => a.Year < 1000 ).OrderBy(a => a.Volcano);
 // Execute Assignment Tasks here!
 // PrintEach(stratovolcanoEruptions, "Stratovolcano eruptions.");
-PrintEach(chileEruption);
+
+// PrintEach(chileEruption);
+// PrintEach(hawaiiIsland);
+// PrintEach(nz1900);
+// PrintEach(over2000);
+// PrintEach(zVolcano);
+// PrintEach(abcVolcano);
+PrintEach(oldAbcVolcano);
 
 // First eruption that is in Chile
 // The lambda here uses a variable 'prod' which represents a product 
